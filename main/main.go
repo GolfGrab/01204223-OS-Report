d75f78276de1153a2e94275438dac239f30b6a9e
GolfGrab <surapus01@gmail.com>
2023-08-12 23:58:32 +0700

    fix money struct

--- main/main.go
+++ main/main.go
@@ -10,34 +10,32 @@ type People struct {
 	say   string
 	money int
 }
+type Money struct {
+	mu    sync.Mutex
+	value int
+}
 
 func main() {
 	var wg sync.WaitGroup
-	var mu sync.Mutex
 	allWorlds := []string{}
-	allMoney := 0
-
+	allMoney := Money{mu: sync.Mutex{}, value: 0}
 	person1 := People{name: "b1", say: "Hello", money: 100}
 	person2 := People{name: "b2", say: "World", money: 200}
 	person3Ptr := &People{name: "b3", say: "!", money: 300}
-
 	wg.Add(3)
-
-	go speak(&person1, &allWorlds, &allMoney, &mu, &wg)
-	go speak(&person2, &allWorlds, &allMoney, &mu, &wg)
-	go speak(person3Ptr, &allWorlds, &allMoney, &mu, &wg)
-
+	go speak(&person1, &allWorlds, &allMoney, &wg)
+	go speak(&person2, &allWorlds, &allMoney, &wg)
+	go speak(person3Ptr, &allWorlds, &allMoney, &wg)
 	wg.Wait()
 	fmt.Println("last word was :", allWorlds)
-	fmt.Println("all money was :", allMoney)
+	fmt.Println("all money was :", allMoney.value)
 }
 
-func speak(person *People, allWorlds *[]string, allMoney *int, mu *sync.Mutex, wg *sync.WaitGroup) {
+func speak(person *People, allWorlds *[]string, allMoney *Money, wg *sync.WaitGroup) {
 	defer wg.Done()
-	mu.Lock()
-	defer mu.Unlock()
-
+	allMoney.mu.Lock()
+	defer allMoney.mu.Unlock()
 	fmt.Printf("%s says: %s\n", person.name, person.say)
 	*allWorlds = append(*allWorlds, person.say)
-	*allMoney += person.money
+	allMoney.value += person.money
 }
