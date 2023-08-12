a4e493284d1bc442dfe42a2f1ebfd3c1acb038eb
GolfGrab <surapus01@gmail.com>
2023-08-12 22:53:23 +0700

    fix typo

--- main/main.go
+++ main/main.go
@@ -17,15 +17,15 @@ func main() {
 
 	person1 := People{name: "b1", say: "Hello"}
 	person2 := People{name: "b2", say: "World"}
-	person3 := new(People)
-	person3.name = "b3"
-	person3.say = "!"
+	person3Ptr := new(People)
+	person3Ptr.name = "b3"
+	person3Ptr.say = "!"
 
 	wg.Add(3)
 
 	go speak(&person1, &mu, &allWorlds, &wg)
 	go speak(&person2, &mu, &allWorlds, &wg)
-	go speak(person3, &mu, &allWorlds, &wg)
+	go speak(person3Ptr, &mu, &allWorlds, &wg)
 
 	wg.Wait()
 	fmt.Println("last word was :", allWorlds)
